68a6be9fef3cb7866cfd57004febf8c77f090124
ale vel <ale.vrs@outlook.com>
2023-08-04 20:31:38 -0500

    agregar header

---
+++ header/header-two/main.js
@@ -0,0 +1,11 @@
+/* Burger Menu */
+function toggleMenu() {
+	let menu = document.querySelector(".menu");
+	menu.classList.toggle("show");
+}
+
+/* Dark mode Icon */
+/* function toggleLight() {
+	let btn = document.querySelector(".dark-mode");
+	btn.classList.toggle("light-mode");
+} */
